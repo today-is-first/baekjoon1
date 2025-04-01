02c563f6a0ddfc3abc58b34fef6a30e56cdeaaca
today-is-first <101533187+today-is-first@users.noreply.github.com>
2025-04-01 16:06:29 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,411 ms, Memory: 106,472 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,59 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution {
-	private static int[] dp;
-	private static boolean[][] arr;
 	private static int N;
-	static boolean[] visited;
+	private static boolean[][] arr; // 자신보다 큰 학생 (정방향)
+	private static boolean[][] arr2; // 자신보다 작은 학생 (역방향)
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int TC = Integer.parseInt(br.readLine());
+
 		for (int tc = 1; tc <= TC; tc++) {
 			N = Integer.parseInt(br.readLine());
 			int M = Integer.parseInt(br.readLine());
+
 			arr = new boolean[N + 1][N + 1];
+			arr2 = new boolean[N + 1][N + 1];
+
 			for (int i = 0; i < M; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				arr[from][to] = true;
+				arr2[to][from] = true;
 			}
 
-			for (int k = 1; k <= N; k++) {
-				for (int i = 1; i <= N; i++) {
-					for (int j = 1; j <= N; j++) {
-						if (arr[i][k] && arr[k][j]) {
-							arr[i][j] = true;
-						}
-					}
-				}
-			}
+			Map<Integer, Set<Integer>> greater = new HashMap<>();
+			Map<Integer, Set<Integer>> smaller = new HashMap<>();
 
-			int cnt = 0;
 			for (int i = 1; i <= N; i++) {
-				int tmp = 0;
-				for (int j = 1; j <= N; j++) {
-					if (i == j)
-						continue;
-					if (arr[i][j] || arr[j][i]) {
-						tmp++;
-					}
-				}
-				if (tmp == N - 1)
-					cnt++;
+				search(i, arr, greater);
+				search(i, arr2, smaller);
 			}
 
-			sb.append("#").append(tc).append(" ").append(cnt).append("\n");
+			int count = 0;
+			for (int i = 1; i <= N; i++) {
+				int total = greater.get(i).size() + smaller.get(i).size();
+				if (total == N - 1) {
+					count++;
+				}
+			}
+
+			sb.append("#").append(tc).append(" ").append(count).append("\n");
 		}
-		System.out.println(sb);
+
+		System.out.print(sb);
 	}
 
+	// DFS로 도달 가능한 정점 Set을 리턴 (중복 제거)
+	static Set<Integer> search(int node, boolean[][] graph, Map<Integer, Set<Integer>> memo) {
+		if (memo.containsKey(node))
+			return memo.get(node);
+
+		Set<Integer> reachable = new HashSet<>();
+		for (int i = 1; i <= N; i++) {
+			if (graph[node][i]) {
+				reachable.add(i);
+				reachable.addAll(search(i, graph, memo));
+			}
+		}
+
+		memo.put(node, reachable);
+		return reachable;
+	}
 }
